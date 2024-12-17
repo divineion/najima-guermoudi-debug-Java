7b04546c46c98412c9ac23cbd455ee8c6a461cf2
divineion <najima.guermoudi@gmail.com>
2024-12-17 09:13:34 +0100

    src/com/hemebiotech/aalytics/AnalyticsCounter.java :  - replace generic "Exception" with specific "IOException" in main() method declaration

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,13 +3,14 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 	
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) throws IOException {
 		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 		while (line != null) {
@@ -31,7 +32,7 @@ public class AnalyticsCounter {
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dilated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
 }
