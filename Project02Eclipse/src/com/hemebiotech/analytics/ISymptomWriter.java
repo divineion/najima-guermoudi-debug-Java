365b5cd2d860cd829bcb1f976d99857a3fe2296d
divineion <najima.guermoudi@gmail.com>
2024-12-18 19:35:47 +0100

    src/com/hemebiotech/analytics/ISymptomWriter :
     - fix method writeSymptoms by adding the "symptoms" parameter.
     - add JavaDoc.

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -1,8 +1,15 @@
 package com.hemebiotech.analytics;
 
+import java.util.Map;
+
 /**
  * Anything that will write symptoms data into a file
  */
 public interface ISymptomWriter {
-	void writeSymptoms();
+	/**
+	 * Writes symptoms and their occurs in a text file
+	 * 
+	 * @param symptoms : a Map of symptom and theirs occurs, alphabetically sorted
+	 */
+	void writeSymptoms(Map<String, Integer> symptoms);
 }
