7ace75ec1ffa9063790d28d4274f248566ce1ee1
divineion <najima.guermoudi@gmail.com>
2024-12-25 18:15:26 +0100

    src/com/hemebiotech/analytics/ReadDataSymptomFromFile.java :
    format code (indent)

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -20,50 +20,54 @@ import com.hemebiotech.analytics.constants.InfoMessages;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+
 	private static Logger logger = LogManager.getLogger(ReadSymptomDataFromFile.class);
-	
+
 	/**
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
-	
+
 	/**
-	 * This method reads the provided text file line by line and adds each line to an ArrayList "result". 
+	 * This method reads the provided text file line by line and adds each line to
+	 * an ArrayList "result".
 	 * 
-	 * @return result, the array containing all symptoms, including possible duplicates
-	 * @throws FileNotFoundException if an attempt to open the file denoted by a specified pathname has failed, 
-	 * 	IOException if any I/O exception occurs. 
+	 * @return result, the array containing all symptoms, including possible
+	 *         duplicates.
+	 * @throws FileNotFoundException if an attempt to open the file denoted by a
+	 *                               specified pathname has failed, IOException if
+	 *                               any I/O exception occurs.
 	 */
 	
 	@Override
 	public List<String> getSymptoms() throws IOException {
 		ArrayList<String> result = new ArrayList<String>();
-		
+
 		if (filepath != null) {
 			try {
 				logger.info(InfoMessages.FILE_READ_ATTEMPT, filepath);
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
 				String line = reader.readLine();
-				
+
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
 				}
-				
+
 				reader.close();
-				
+
 			} catch (FileNotFoundException e) {
 				logger.error(ErrorMessages.FILE_NOT_FOUND, filepath);
-				
+
 			} catch (IOException e) {
-				logger.error(e.getMessage());			
+				logger.error(e.getMessage());
 			}
 		}
-				
+
 		return result;
 	}
 }
