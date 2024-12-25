d1408468f68487b1954f7e48becb0e16209fe81d
divineion <najima.guermoudi@gmail.com>
2024-12-25 11:50:00 +0100

    src/com/hemebiotech/analytics/AnalyticsCounter.java :
    add JavaDoc.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,20 +11,44 @@ import org.apache.logging.log4j.Logger;
 
 import com.hemebiotech.analytics.constants.ErrorMessages;
 
+/**
+ * This class processes symptom data by reading, counting, ordering and writing the results
+ * to an output.
+ */
 public class AnalyticsCounter {
 	private static Logger logger = LogManager.getLogger(AnalyticsCounter.class);
+	
 	private ISymptomReader reader;
 	private ISymptomWriter writer;
 	
+	/**
+	 * Constructs an AnalyticsCounter instance with the provided reader and writer implementations.
+	 * 
+	 * @param reader an implementation of {@link ISymptomReader} to read  symptom data.
+	 * @param writer an implementation of {@link ISymptomWriter} to write results. 
+	 */
 	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
 		this.reader = reader;
 		this.writer = writer;
 	}
 	
+	/**
+	 * Retrieves the raw list of symptoms from the data source defined by the reader.
+	 * Each symptom is included as many times as it is reported in the source file.
+	 * 
+	 * @return a {@link List} of {@link String} where each element represents a symptom.
+	 * @throws IOException in an I/O error occurs while reading the data source. 
+	 */
 	public List<String> getSymptoms() throws IOException {
 		return reader.getSymptoms();
 	}
 	
+	/**
+	 * Counts the occurrences of each symptom in the provided list. 
+	 * 
+	 * @param symptoms a {@link List} of {@link String} representing the raw symptoms data. 
+	 * @return a {@link Map} where keys are symptoms as {@link String} and values are occurrences as {@link Integer}.
+	 */
 	public Map<String, Integer> countSymptoms(List<String> symptoms) {
 		if (symptoms == null) {
 			logger.error(ErrorMessages.SYMPTOMS_LIST_NULL);
@@ -43,11 +67,23 @@ public class AnalyticsCounter {
 		return symptomOccurrences;
 	}
 	
+	 /**
+     * Sorts the symptoms alphabetically by their names.
+     *
+     * @param symptoms a {@link Map} of symptoms and their occurrences.
+     * @return a new {@link TreeMap} where the symptoms are automatically sorted in alphabetical order.
+     */
 	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
 		Map<String, Integer> sortedSymptoms = new TreeMap<>(symptoms);
 		return sortedSymptoms; 
 	}
 	
+	  /**
+     * Writes the sorted symptoms and their occurrences to the output specified by the writer.
+     *
+     * @param symptoms a {@link Map} of sorted symptoms and their occurrences.
+     * @throws IOException if an error occurs while writing the data.
+     */
 	public void writeSymptoms(Map<String, Integer> symptoms) throws IOException {
 		writer.writeSymptoms(symptoms);
 	}
